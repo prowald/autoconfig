0ec2a971101b9f6ddce6db1535a8f0dc8566f1b4
prowald <47645474+prowald@users.noreply.github.com>
2021-01-30 20:38:34 +0100

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -15,10 +15,10 @@ while true; do
 				then
 					echo -e ${newhost,} > /etc/hostname
 					sed "s/127.0.1.1.*/127.0.1.1	${newhost,}/g"
-					exit 2
+					break 2
 				else
 					echo "Only numbers and letters are allowed."
-					exit 
+					break
 				fi;;
         * ) break;;
     esac
