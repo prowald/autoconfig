de4f4d8aa62d0651b66ed7e9f8f7584f5efc7fec
prowald <47645474+prowald@users.noreply.github.com>
2021-01-30 19:36:32 +0100

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+RESET='\033[0m'
+COLOR='\e[49m'
 
 hostname=$(uname -n)
-echo -e "\e[49mCurrent Hostname: \e[94m$(hostname)"
-echo -e "\e[49m"
+echo -e "$(COLOR)Current Hostname: \e[94m$(hostname)$(RESET)"
+echo "Do you wish to change the Hostname?"
+select yn in "Yes" "No"
+case $yn in
+    Yes ) make install;;
+    No ) exit;;
+esac
 read -p "New Hostname: " newhost
+echo -e "$(COLOR)Current Hostname: \e[94m$(hostname)$(RESET)"
