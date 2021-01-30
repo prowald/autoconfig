a30fa73e433ae1670299260fc53876675797bf4e
prowald <47645474+prowald@users.noreply.github.com>
2021-01-30 20:50:28 +0100

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -4,7 +4,7 @@ COLOR='\e[94m'
 
 hostname=$(uname -n)
 newhost=
-echo -e "${RESET}Current Hostname: ${COLOR}$(hostname)${RESET}"
+echo -e "${RESET}Current Hostname: ${COLOR}${hostname}${RESET}"
 while true; do
     read -p "Change Hostname?(yN)" yn
     case $yn in
@@ -15,7 +15,7 @@ while true; do
 				then
 					echo -e ${newhost,} > /etc/hostname
 					sed -i "s/127.0.1.1.*/127.0.1.1	${newhost,}/g" /etc/hosts
-					echo -e "Hostname successfully changed to ${COLOR} ${newhost,} ${RESET} "
+					echo -e "Hostname successfully changed to ${COLOR} $(newhost,) ${RESET} "
 					break 3
 				else
 					echo "Only numbers and letters are allowed."
